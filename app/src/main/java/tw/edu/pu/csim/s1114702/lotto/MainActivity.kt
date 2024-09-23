e541257887ff97ebacb2762baa83403a51a02e8d
liyi2b <janis5201023@gmail.com>
2024-09-23 16:00:05 +0800

    第二次 Commit

--- app/src/main/java/tw/edu/pu/csim/s1114702/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114702/lotto/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.csim.s1114702.lotto
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = "0"
     }
 }
